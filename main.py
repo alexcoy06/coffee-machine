a6d2786873daeccb19d2425e70bf3117650908d9
Alexander Coy <alexcoy06@gmail.com>
2025-01-30 22:00:08 -0600

    Requirements completed,  Basic math functions completed

--- main.py
+++ main.py
@@ -8,30 +8,35 @@ drink_choices = {
     'cappuccino': {
         'coffee': 24,
         'milk': 100,
-        'water': 250
+        'water': 250,
+        'cost': 3
     },
     'espresso': {
         'coffee': 18,
         'milk': 0,
-        'water': 50
+        'water': 50,
+        'cost': 1.5
     },
     'latte': {
         'coffee': 24,
         'milk': 150,
-        'water': 200
+        'water': 200,
+        'cost': 2.5
     }
 }
 
 
 # Machine Functions 
 machine_running = True
-cash_balance = 10
+cash_balance = 0
 machine_log = []
 
-penny = 0
-nickel = 0
-dime = 0
-quarter = 0
+money = {
+    'quarters': 0,
+    'dimes': 0,
+    'nickels': 0,
+    'pennies': 0
+}
 
 
 # Coded Function
@@ -63,8 +68,43 @@ def update_resources(choice, drink_choices=drink_choices):
         machine_log.append(choice + '.failed')
     return drink_choices, machine_log
 
-def buy_coffee():
-    return
+def buy_coffee(choice, money=money, cash_balance=cash_balance):
+    cost = drink_choices[choice]['cost']
+    paid = 0
+    change = -1
+    not_enough_money = True
+    
+    while not_enough_money:
+        count = {
+            'quarters': 0,
+            'dimes': 0,
+            'nickels': 0,
+            'pennies': 0    
+        }
+        
+        print(f'Your {choice.capitalize()} will cost ${cost:.2f}. Please insert coins.')
+        for coin in money:
+            count[coin] = int(input(f'How many {coin}: '))
+        
+        paid =  (0.1 *count['dimes']) + (0.05 * count['nickels']) + (0.01 * count['pennies']) + (0.25 * count['quarters'])
+            
+        if paid >= cost:
+            money[coin] += count[coin] # plan to come back and do more with this
+            change = paid - cost
+            cash_balance += cost - change
+            print(f'Your change is ${change:.2f}')
+            not_enough_money = False
+            
+        else:
+            attempt = input(f'Insufficient funds, "try again" or "cancel": ').lower()
+            if attempt == 'cancel':
+                not_enough_money = False
+            elif attempt == 'try again':
+                pass
+            else:
+                print('Wrong input.')
+            
+    return money, cash_balance
 
 
 #Machine Operation
@@ -79,10 +119,12 @@ while machine_running:
     if choice == 'off':
         machine_running = False
     elif choice == 'report':
-        machine_log.append(choice)
         report()
+        machine_log.append(choice)
     else:
         drink_choices, machine_log = update_resources(choice)
         
-    
+    if (choice in drink_choices) & ('.failed' not in machine_log[-1]):
+        money, cash_balance = buy_coffee(choice)
         
+    # Add 'out of service' task did not require but i want to at some point
