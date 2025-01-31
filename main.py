3ce89f53f9246621b3fcd3f227c443a70ac32be2
Alexander Coy <alexcoy06@gmail.com>
2025-01-30 19:14:57 -0600

    Basic Coffee Machine Operation

--- main.py
+++ main.py
@@ -1,39 +1,88 @@
 # Coffee
-water = 1250
-coffee = 120
-milk = 750
+drink_choices = {
+    'resources': {
+        'coffee': 120,
+        'milk': 750,
+        'water': 1250
+    },
+    'cappuccino': {
+        'coffee': 24,
+        'milk': 100,
+        'water': 250
+    },
+    'espresso': {
+        'coffee': 18,
+        'milk': 0,
+        'water': 50
+    },
+    'latte': {
+        'coffee': 24,
+        'milk': 150,
+        'water': 200
+    }
+}
 
-CAPPUCCINO = {
-    'price': 3,
-    'water': 250,
-    'coffee': 24, 
-    'milk': 100
-}
-ESPRESSO = {
-    'price': 3,
-    'water': 250,
-    'coffee': 24, 
-    'milk': 100
-}
-LATTE = {
-    'price': 3,
-    'water': 250,
-    'coffee': 24, 
-    'milk': 100
-}
 
 # Machine Functions 
-coffee_machine = True
+machine_running = True
 cash_balance = 10
-drink_log = ()
+machine_log = []
 
 penny = 0
 nickel = 0
 dime = 0
 quarter = 0
 
-# Prompt
-choice = input('Thank you for choosing Coy\'s Coin Coffee Machine! We proudly serve espresso, latte, and cappuccino.What would you like? ').lower()
 
-while (choice != 'espresso') & (choice != 'latte') & (choice != 'cappuccino'):
-    choice = input('Invalid input. Please enter one of the following: espresso, latte, or cappuccino.')
+# Coded Function
+def report(resources=drink_choices):
+    for resource, amount in resources['resources'].items():
+        if resource == 'coffee':
+            print(f'{resource.capitalize()}: {amount}gm')
+        else:
+            print(f'{resource.capitalize()}: {amount}ml')
+    print(f'Cash Balance: {cash_balance}\nMachine Log: {", ".join(machine_log)}')
+    
+def update_resources(choice, drink_choices=drink_choices):
+    drink = drink_choices[choice]
+    resources = drink_choices['resources']
+    drink_failed = 0
+    
+    for resource in resources:
+        if resources[resource] < drink[resource]:
+            drink_failed = 1
+            print(f'Sorry, not enough {resource}.')
+                
+            
+    if drink_failed == 0:
+        resources['coffee'] -= drink['coffee']
+        resources['milk'] -= drink['milk']
+        resources['water'] -= drink['water']
+        machine_log.append(choice)
+    else:
+        machine_log.append(choice + '.failed')
+    return drink_choices, machine_log
+
+def buy_coffee():
+    return
+
+
+#Machine Operation
+while machine_running:
+    # Prompt
+    choice = input('Thank you for choosing Coy\'s Coin Coffee Machine! We proudly serve espresso, latte, and cappuccino.What would you like? ').lower()
+    
+    # Input Controls 
+    while (choice != 'espresso') & (choice != 'latte') & (choice != 'cappuccino') & (choice != 'report') & (choice != 'off'):
+        choice = input('Invalid input. Please enter one of the following: espresso, latte, or cappuccino.')
+    
+    if choice == 'off':
+        machine_running = False
+    elif choice == 'report':
+        machine_log.append(choice)
+        report()
+    else:
+        drink_choices, machine_log = update_resources(choice)
+        
+    
+        
